7e1d057c5093359cc6207611787e284a1eed3ff1
Patrick Bucher <patrick.bucher@mailbox.org>
2025-04-11 14:13:40 +0200

    exception handling

--- src/exception-handling.test.ts
+++ src/exception-handling.test.ts
@@ -1,5 +1,20 @@
 import { fail } from "./exception-handling";
 
 test("test fail", () => {
-  expect(fail("Joe")).toThrow("Joe is a complete failure.");
+  expect(() => fail("Joe")).toThrow("Joe is a complete failure.");
 });
+
+function expect(something) {
+    return {
+        toBe(what): {
+            return something == what
+        }
+        toThrow(what): {
+            try {
+                something();
+            } catch (somethingElse) {
+                what == somethingElse
+            }
+        }
+    };
+}
