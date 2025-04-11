2561f250cac5d756204c71d1770efdb9c37213e3
Patrick Bucher <patrick.bucher@mailbox.org>
2025-04-11 10:43:09 +0200

    filter, map, reduce, loops

--- src/filter-map-reduce-loops.ts
+++ src/filter-map-reduce-loops.ts
@@ -1,5 +1,4 @@
 export function sumSquareOfOddNumbers(xs: number[]): number {
-  // TODO: refactor into single steps with type annotations
   return xs
     .filter((x) => x % 2 == 1)
     .map((x) => x ** 2)
@@ -7,6 +6,11 @@ export function sumSquareOfOddNumbers(xs: number[]): number {
 }
 
 export function mutliplyIncrementOfEvenNumbers(xs: number[]): number {
-  // TODO: implement using loops
-  return 0;
+    let acc = 1;
+    for (const x of xs) {
+        if (x % 2 === 0) {
+            acc *= (x + 1);
+        }
+    }
+    return acc;
 }
