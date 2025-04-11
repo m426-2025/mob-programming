b06bd566589de0cf2494c118a654b40c5e4e1046
Patrick Bucher <patrick.bucher@mailbox.org>
2025-04-11 14:56:50 +0200

    maps

--- src/maps.test.ts
+++ src/maps.test.ts
@@ -3,30 +3,11 @@ import { assignPoints } from "./maps";
 test("Formula 1 Ranking", () => {
   // Arrange
   const points1991to2002: Map<number, number> = new Map([
-    [1, 10],
-    [2, 6],
-    [3, 4],
-    [4, 3],
-    [5, 2],
-    [6, 1],
+    [1, 10], [2, 6], [3, 4], [4, 3], [5, 2], [6, 1],
   ]);
   const results: string[][] = [
-    [
-      "M. Schumacher",
-      "R. Barrichello",
-      "D. Coulthard",
-      "M. Häkkinen",
-      "A. Wurz",
-      "J. Villeneuve",
-    ],
-    [
-      "M. Schumacher",
-      "M. Häkkinen",
-      "R. Barrichello",
-      "R. Schumacher",
-      "A. Wurz",
-      "D. Coulthard",
-    ],
+    [ "M. Schumacher", "R. Barrichello", "D. Coulthard", "M. Häkkinen", "A. Wurz", "J. Villeneuve", ],
+    [ "M. Schumacher", "M. Häkkinen", "R. Barrichello", "R. Schumacher", "A. Wurz", "D. Coulthard", "J. Verstappen"],
   ];
   const expected: Map<string, number> = new Map([
     ["M. Schumacher", 20],
@@ -36,6 +17,7 @@ test("Formula 1 Ranking", () => {
     ["A. Wurz", 4],
     ["R. Schumacher", 3],
     ["J. Villeneuve", 1],
+    ["J. Verstappen", 0],
   ]);
 
   // Act
--- src/maps.ts
+++ src/maps.ts
@@ -1,8 +1,13 @@
-// TODO: think of map example
 export function assignPoints(
   results: string[][],
   pointsByRank: Map<number, number>,
 ): Map<string, number> {
-  // TODO: implement
-  return new Map();
+  let placements: Map<string, number> = new Map();
+  for (let race of results) {
+      for (let i = 0; i < race.length; i++) {
+          let lastPoints: number = placements.get(race[i]) || 0;
+          placements.set(race[i], (pointsByRank.get(i + 1) || 0) + lastPoints);
+      }
+  }
+  return placements;
 }
