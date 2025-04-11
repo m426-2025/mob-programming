5c9733cf6d95a7be3c33384c930c2be7419a2a9a
Patrick Bucher <patrick.bucher@mailbox.org>
2025-04-11 11:11:22 +0200

    maps

--- src/maps.test.ts
+++ src/maps.test.ts
@@ -11,31 +11,12 @@ test("Formula 1 Ranking", () => {
     [6, 1],
   ]);
   const results: string[][] = [
-    [
-      "M. Schumacher",
-      "R. Barrichello",
-      "D. Coulthard",
-      "M. Häkkinen",
-      "A. Wurz",
-      "J. Villeneuve",
-    ],
-    [
-      "M. Schumacher",
-      "M. Häkkinen",
-      "R. Barrichello",
-      "R. Schumacher",
-      "A. Wurz",
-      "D. Coulthard",
-    ],
+    [ "M. Schumacher", "R. Barrichello", "D. Coulthard", "M. Häkkinen", "A. Wurz", "J. Villeneuve", ],
+    [ "M. Schumacher", "M. Häkkinen", "R. Barrichello", "R. Schumacher", "A. Wurz", "D. Coulthard", ],
   ];
   const expected: Map<string, number> = new Map([
-    ["M. Schumacher", 20],
-    ["R. Barrichello", 10],
-    ["M. Häkkinen", 9],
-    ["D. Coulthard", 5],
-    ["A. Wurz", 4],
-    ["R. Schumacher", 3],
-    ["J. Villeneuve", 1],
+    ["M. Schumacher", 20], ["R. Barrichello", 10], ["M. Häkkinen", 9], ["D. Coulthard", 5],
+    ["A. Wurz", 4], ["R. Schumacher", 3], ["J. Villeneuve", 1],
   ]);
 
   // Act
--- src/maps.ts
+++ src/maps.ts
@@ -1,8 +1,16 @@
-// TODO: think of map example
 export function assignPoints(
   results: string[][],
   pointsByRank: Map<number, number>,
 ): Map<string, number> {
-  // TODO: implement
-  return new Map();
+    const championship = new Map();
+    for (const race of results) {
+        for (let index = 0; index < race.length; index++) {
+            const driver = race[index];
+            const rank = index + 1;
+            const points = pointsByRank.get(rank) || 0;
+            const currentPoints = championship.get(driver) || 0;
+            championship.set(driver, points + currentPoints);
+        }
+    }
+  return championship;
 }
